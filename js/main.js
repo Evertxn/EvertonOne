2dd637d25503869b5ad09b1b61195ce91e795601
EVERTON B WEBB <Ev@EVERTONs-MacBook-Pro.local>
2017-08-29 18:40:11 -0400

    tweaked projects and links added

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function main() {
     	// will fade out the whole DIV that covers the website. 
     	$("#preloader").delay(500).fadeOut("slow").remove();      
 
-  	}) 
+  	});
 
    // Page scroll
   	$('a.page-scroll').click(function() {
@@ -59,7 +59,7 @@ function main() {
     $('body').scrollspy({ 
         target: '.navbar-default',
         offset: 80
-    })
+    });
 
   	$(document).ready(function() {
   	    $("#testimonial").owlCarousel({
